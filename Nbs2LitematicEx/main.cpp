1f487a5e0d69661d8695bd957322e65bc2f9268c
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-03-31 22:30:22 +0800

    1

--- Nbs2LitematicEx/main.cpp
+++ Nbs2LitematicEx/main.cpp
@@ -2,31 +2,42 @@
 #include <nbs_cpp/NBS_All.hpp>
 
 #include <vector>
+#include <stdint.h>
 
 struct MyNote
 {
-
-
-
-
-
+	NBS_File::LONG tick;
+	NBS_File::BYTE instrument;
+	NBS_File::BYTE key;
 };
 
-using ListMyNote = std::vector<MyNote>;
+using MyNoteList = std::vector<MyNote>;
 
-
-ListMyNote ToMyNoteList(const NBS_File &f)
+MyNoteList ToMyNoteList(const NBS_File &fNBS)
 {
+	std::vector<uint8_t> blistLocked;
+	blistLocked.reserve(fNBS.listLayer.size());
+	for (auto &it : fNBS.listLayer)
+	{
+		blistLocked.push_back(it.lock);
+	}
 
+	MyNoteList listMyNote;
+	listMyNote.reserve(fNBS.listNote.size());
+	for (auto &it : fNBS.listNote)
+	{
+		if (it.layer < blistLocked.size() && blistLocked[it.layer] == 0)//没有溢出范围且未上锁（静音）
+		{
+			listMyNote.emplace_back(it.tick, it.instrument, it.key);
+		}
+	}
 
-
-
-
+	return listMyNote;
 }
 
 
 
-int main(void)
+int main(int argc, char *argv[])
 {
 
 
