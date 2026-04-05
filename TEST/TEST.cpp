03a7e16343a75711b12f9ffd5813f3a7405fa94f
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-04-05 09:05:00 +0800

    1

--- TEST/TEST.cpp
+++ TEST/TEST.cpp
@@ -12,7 +12,7 @@ int main(void)
 	}
 
 	const auto ret = DoublingCountingRadixSortSuffixArray(INT8_MAX, strInput);
-	const auto ret2 = HegihtArray(strInput, ret);
+	const auto ret2 = HeightArray(strInput, ret);
 
 	for (auto &it : ret.vSuffixArray)
 	{
