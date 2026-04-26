9eebec7d0a81a8f348b7fbe4867a8fad35c42519
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-04-26 10:19:11 +0800

    1

--- LitematicGenerator/LitematicGenerator.hpp
+++ LitematicGenerator/LitematicGenerator.hpp
@@ -5,5 +5,51 @@
 #include <nbt_cpp/NBT_All.hpp>
 #include <nbs_cpp/NBS_All.hpp>
 
+#include <vector>
+
+struct LitematicFile
+{
+public:
+	struct MetaData
+	{
 
 
+
+
+	public:
+		NBT_Type::Compound ToCompound(void)
+		{
+			return {};//TODO
+		}
+
+	};
+
+	struct Region
+	{
+
+
+
+	public:
+		NBT_Type::Compound ToCompound(void)
+		{
+			return {};//TODO
+		}
+
+	};
+
+	using Regions = std::vector<Region>;
+
+public:
+	MetaData stMetaData;
+	Regions listRegion;
+	NBT_Type::Int iMinecraftDataVersion = 3465;//1.20.1 -> 3465
+	NBT_Type::Int iVersion = 6;//Litematic Version
+	NBT_Type::Int iSubVersion = 1;//Litematic SubVersion
+
+public:
+	NBT_Type::Compound ToCompound(void)
+	{
+		return {};//TODO
+	}
+
+};
