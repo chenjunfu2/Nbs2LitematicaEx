d094a19fe6279d6c66a90b83bd64b8eeb0a7a074
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-04-26 15:33:47 +0800

    1

--- LitematicGenerator/LitematicGenerator.hpp
+++ LitematicGenerator/LitematicGenerator.hpp
@@ -19,9 +19,14 @@ public:
 		NBT_Type::Int z;
 
 	public:
-		NBT_Type::Compound ToCompound(void)
+		NBT_Type::Compound ToCompound(void) const//builtin无需move特化
 		{
-			return {};//TODO
+			return NBT_Type::Compound
+			{
+				{MU8STR("x"), x},
+				{MU8STR("y"), y},
+				{MU8STR("z"), z},
+			};
 		}
 	};
 
@@ -39,11 +44,37 @@ public:
 		NBT_Type::Int iTotalVolume;
 
 	public:
-		NBT_Type::Compound ToCompound(void)
+		NBT_Type::Compound ToCompound(void) const &
 		{
-			return {};//TODO
+			return NBT_Type::Compound
+			{
+				{MU8STR("EnclosingSize"),	stEnclosingSize.ToCompound()},
+				{MU8STR("Author"),			strAuthor},
+				{MU8STR("Description"),		strDescription},
+				{MU8STR("Name"),			strName},
+				{MU8STR("RegionCount"),		iRegionCount},
+				{MU8STR("TimeCreated"),		lTimeCreated},
+				{MU8STR("TimeModified"),	lTimeModified},
+				{MU8STR("TotalBlocks"),		iTotalBlocks},
+				{MU8STR("TotalVolume"),		iTotalVolume},
+			};
 		}
 
+		NBT_Type::Compound ToCompound(void) &&
+		{
+			return NBT_Type::Compound
+			{
+				{MU8STR("EnclosingSize"),	std::move(stEnclosingSize).ToCompound()},
+				{MU8STR("Author"),			std::move(strAuthor)},
+				{MU8STR("Description"),		std::move(strDescription)},
+				{MU8STR("Name"),			std::move(strName)},
+				{MU8STR("RegionCount"),		iRegionCount},//builtin无需move
+				{MU8STR("TimeCreated"),		lTimeCreated},
+				{MU8STR("TimeModified"),	lTimeModified},
+				{MU8STR("TotalBlocks"),		iTotalBlocks},
+				{MU8STR("TotalVolume"),		iTotalVolume},
+			};
+		}
 	};
 
 	struct Region
@@ -59,9 +90,34 @@ public:
 		NBT_Type::LongArray larrBlockStates;
 
 	public:
-		NBT_Type::Compound ToCompound(void)
+		NBT_Type::Compound ToCompound(void) const &
 		{
-			return {};//TODO
+			return NBT_Type::Compound
+			{
+				{MU8STR("Position"),			stPosition.ToCompound()},
+				{MU8STR("Size"),				stSize.ToCompound()},
+				{MU8STR("BlockStatePalette"),	listBlockStatePalette},
+				{MU8STR("Entities"),			listEntities},
+				{MU8STR("PendingBlockTicks"),	listPendingBlockTicks},
+				{MU8STR("PendingFluidTicks"),	listPendingFluidTicks},
+				{MU8STR("TileEntities"),		listTileEntities},
+				{MU8STR("BlockStates"),			larrBlockStates},
+			};
+		}
+
+		NBT_Type::Compound ToCompound(void) &&
+		{
+			return NBT_Type::Compound
+			{
+				{MU8STR("Position"),			std::move(stPosition).ToCompound()},
+				{MU8STR("Size"),				std::move(stSize).ToCompound()},
+				{MU8STR("BlockStatePalette"),	std::move(listBlockStatePalette)},
+				{MU8STR("Entities"),			std::move(listEntities)},
+				{MU8STR("PendingBlockTicks"),	std::move(listPendingBlockTicks)},
+				{MU8STR("PendingFluidTicks"),	std::move(listPendingFluidTicks)},
+				{MU8STR("TileEntities"),		std::move(listTileEntities)},
+				{MU8STR("BlockStates"),			std::move(larrBlockStates)},
+			};
 		}
 
 	};
@@ -72,9 +128,26 @@ public:
 		std::unordered_map<NBT_Type::String, Region> mapRegion;
 
 	public:
-		NBT_Type::Compound ToCompound(void)
+		NBT_Type::Compound ToCompound(void) const &
 		{
-			return {};//TODO
+			NBT_Type::Compound cpdRet;
+			for (const auto &[regionName, regionData] : mapRegion)
+			{
+				cpdRet.PutCompound(regionName, regionData.ToCompound());
+			}
+			return cpdRet;
+		}
+
+		NBT_Type::Compound ToCompound(void) &&
+		{
+			NBT_Type::Compound cpdRet;
+			for (auto &[regionName, regionData] : mapRegion)
+			{
+				cpdRet.PutCompound(regionName, std::move(regionData).ToCompound());
+			}
+
+			mapRegion.clear();//move后需要清理空键值
+			return cpdRet;
 		}
 
 	};
@@ -84,13 +157,32 @@ public:
 	MetaData stMetaData;
 	Regions stRegions;
 	NBT_Type::Int iMinecraftDataVersion = 3465;//1.20.1 -> 3465
-	NBT_Type::Int iVersion = 6;//Litematic Version
 	NBT_Type::Int iSubVersion = 1;//Litematic SubVersion
+	NBT_Type::Int iVersion = 6;//Litematic Version
 
 public:
-	NBT_Type::Compound ToCompound(void)
+	NBT_Type::Compound ToCompound(void) const &
 	{
-		return {};//TODO
+		return NBT_Type::Compound
+		{
+			{MU8STR("Metadata"),				stMetaData.ToCompound()},
+			{MU8STR("Regions"),					stRegions.ToCompound()},
+			{MU8STR("MinecraftDataVersion"),	iMinecraftDataVersion},
+			{MU8STR("SubVersion"),				iSubVersion},
+			{MU8STR("Version"),					iVersion},
+		};
+	}
+
+	NBT_Type::Compound ToCompound(void) &&
+	{
+		return NBT_Type::Compound
+		{
+			{MU8STR("Metadata"),				std::move(stMetaData).ToCompound()},
+			{MU8STR("Regions"),					std::move(stRegions).ToCompound()},
+			{MU8STR("MinecraftDataVersion"),	iMinecraftDataVersion},//builtin无需move
+			{MU8STR("SubVersion"),				iSubVersion},
+			{MU8STR("Version"),					iVersion},
+		};
 	}
 
 };
